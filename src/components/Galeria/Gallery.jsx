2f9e1ba4f23291f6181d7b82a4e823943e2dc8ce
Dammi <damianchoo78@gmail.com>
2024-09-15 20:35:12 -0300

    componente obra creado

--- src/components/Galeria/Gallery.jsx
+++ src/components/Galeria/Gallery.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./gallery.css";
-
+import { Obra } from "../Obras/Obra";
 export function Gallery() {
     //----------------------FX QUE GIRA LA HABITACION-----------------------------------
     const [rotacion, setRotacion] = useState(0);
@@ -14,38 +14,31 @@ export function Gallery() {
     return (
         <div className="contenedor">
             <div className="habitacion" style={{ transform: `rotateY(${rotacion}deg)` }}>
+
                 <div className="pared-frontal">
-                    <div className="flechas">
-                        <div id="arrow-left" onClick={girarIzquierda}></div>
-                        <div id="arrow-right" onClick={girarDerecha}></div>
-                    </div>
+                    <Obra></Obra>
                 </div>
+
                 <div className="pared pared-trasera">
-                    <div className="flechas">
-                        <div id="arrow-left" onClick={girarIzquierda}>hoalsdls</div>
-                        <div id="arrow-right" onClick={girarDerecha}></div>
-                    </div>
+                    <Obra></Obra>
                 </div>
+
                 <div className="pared pared-derecha">
-                    <div className="flechas">
-                        <div id="arrow-left" onClick={girarIzquierda}></div>
-                        <div id="arrow-right" onClick={girarDerecha}></div>
-                    </div>
+                    <Obra></Obra>
                 </div>
+
                 <div className="pared pared-izquierda">
-                    <div className="flechas">
-                        <div id="arrow-left" onClick={girarIzquierda}></div>
-                        <div id="arrow-right" onClick={girarDerecha}></div>
-                    </div>
+                    <Obra></Obra>
                 </div>
+
                 {/* -----REALMENTE NO ES NECESARIO EL TECHO Y SUELO-------- */}
                 {/* <div className="suelo"></div>
                 <div className="techo"></div> */}
             </div>
 
             <div className="controles">
-                <button onClick={girarIzquierda}>Izquierda</button>
-                <button onClick={girarDerecha}>Derecha </button>
+                <div onClick={girarIzquierda} id="arrow-left"></div>
+                <div onClick={girarDerecha} id="arrow-right"></div>
             </div>
         </div>
     );
---
+++ src/components/Obras/Obra.jsx
@@ -0,0 +1,11 @@
+import "./obra.css";
+
+export function Obra (){
+    return(
+        <div className="contenedor_obras">
+            <div className="obra"></div>
+            <div className="obra"></div>
+            <div className="obra"></div>
+        </div>
+    );
+}
